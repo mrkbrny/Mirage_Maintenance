ccca44408008c35ef4b4b1fb8512c9f85b631b00
Baranyai Márk <baranyai.mark@gmail.com>
2024-08-11 22:04:40 +0200

    refactor and error handling

--- mirage_maintenance.py
+++ mirage_maintenance.py
@@ -1,52 +1,58 @@
 def is_all_zero(list):
+    #check if all values in list equals to 0, return False as soon as a non-0 value is found
     for num in list:
         if num != 0:
             return False
     return True
 
-#history = [[0, 3, 6, 9, 12, 15]]
-#history = [[1, 3, 6, 10, 15, 21]]
-#history = [[10, 13, 16, 21, 30, 45]]
-#history = [[-3, 8, 35, 82, 152, 255, 432, 803, 1648, 3531, 7478, 15221, 29521, 54584, 96585, 164316, 269975, 430114, 666765, 1008764, 1493294]]
-#history = [[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]]
+def process_history(readings):
+    sequences = [readings]
+    #first, build array layers until all the values of the last row equals to 0 
+    reading_counter = 0
+    depth = 0 #counter for sequence layer
+    while not is_all_zero(sequences[-1]):
+        sequences.append([])
+        while reading_counter < len(sequences[depth]) - 1:
+            sequences[depth+1].append(sequences[depth][reading_counter + 1] - sequences[depth][reading_counter])
+            reading_counter += 1
+        reading_counter = 0
+        depth += 1
 
-sum = 0
-with open('input.txt') as f:
-    while line := f.readline():
-        input = line.split(" ")
-        
-        if "\n" in line:
-            #remove last character of the last value, it should be \n for new line
-            input[-1] = input[-1][:-1]
-        else:
-            print("asd")
-        
-        history = [[]]
+    #second, add a 0 at the end for placeholder for all layers
+    for layer in sequences:
+        layer.append(0)
+
+    #lastly, calculate correct values placeholder values from bottom to top, until we reach the first layer
+    depth = len(sequences) - 2
+    while depth >= 0:
+        sequences[depth][-1] = sequences[depth][-2] + sequences[depth+1][-1] 
+        depth -= 1
+    
+    return sequences[0][-1]
+
+def process_input(line):
+    #sanitize and format input to expected format   
+    input = line      
+    if "\n" in input:
+        #remove last character of input, it could be \n for new line
+        input = input[:-1]
+    input = input.split(" ")
+
+    history = []
+    try:
         for val in input:
-            history[0].append(int(val))
-        #print(history)
+            history.append(int(val))
+    except:
+        print("failed to process line: ", line)
+    return history
 
-        i = 0
-        depth = 0
-        #print(is_all_zero(history[-1]))
-        while not is_all_zero(history[-1]):
-            history.append([])
-            while i < len(history[depth]) - 1:
-                history[depth+1].append(history[depth][i + 1] - history[depth][i])
-                i += 1
-            #print(history[-1])
-            i = 0
-            depth += 1
-
-        for line in history:
-            line.append(0)
-
-        depth = len(history) - 2
-        while depth >= 0:
-            history[depth][-1] = history[depth][-2] + history[depth+1][-1] 
-            #print(history[depth])
-            depth -= 1
-        print(history[0][-1])
-        sum += history[0][-1]
-
-print(sum)
+#process each line in input.txt
+input_file = 'input.txt'
+sum = 0
+try:
+    with open(input_file) as f:
+        while line := f.readline():
+            sum += process_history(process_input(line))
+    print(sum)
+except:
+    print("error with file input")
